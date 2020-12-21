a8831f1f54a8905d2dd43da83a6aa55873f8225e
absucc <57920717+absucc@users.noreply.github.com>
2020-12-21 18:49:37 -0300

    Alpha 3.1

--- bot.py
+++ bot.py
@@ -112,11 +112,11 @@ async def info(ctx, category):
   elif category == "more":
     embed=discord.Embed(title="More", description="", color=botcolor)
     embed.add_field(name=prefix + "chucknorris", value="Random Chuck Norris Joke", inline=True)
-    embed.add_field(name=prefix + "say <words>", value="The bot something for you", inline=True)
-    embed.add_field(name=prefix + "hug", value="A hug, for you, my friend", inline=True)
     embed.add_field(name=prefix + "delete <amount>", value="Delete message", inline=True)
+    embed.add_field(name=prefix + "hug", value="A hug, for you, my friend", inline=True)
+    embed.add_field(name=prefix + "say <words>", value="The bot something for you", inline=True)
     embed.add_field(name=prefix + "purge <amount>", value="Delete message", inline=True)
-    #embed.add_field(name=prefix + "ft <language code> <text>", value="Fun Translations (https://funtranslations.com) \n For more info of the language codes enter here: https://funtranslations.com/api", inline=True)
+    embed.add_field(name=prefix + "xkcd <latest/last/current/number>", value="Read a comic of xkcd! IN APICORD!!!", inline=True)
     await ctx.send(embed=embed)
   else:
     await ctx.send("error, please put a valid command")
@@ -128,48 +128,56 @@ async def img(ctx, *, something):
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Here you have your coffee"), description=(""), color=(botcolor))
     embed.set_image(url=(jsdata['file']))
+    embed.set_footer(text="Powered by **Coffee API**")
     await ctx.send(embed=embed)
   elif something == "dog":
     jdat = (http.request('GET', 'https://dog.ceo/api/breeds/image/random'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("A doggo! :D"), description=("I wish you'll like this uwu"), color=(botcolor))
     embed.set_image(url=(jsdata['message']))
+    embed.set_footer(text="Powered by Dog CEO")
     await ctx.send(embed=embed)
   elif something == "cat":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/cat'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("A cat! :D"), description=("kittens & adult kittens"), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif something == "panda":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/panda'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("p a n d a"), description=(""), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif something == "red panda":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/red_panda'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("red panda :)"), description=(""), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif something == "bird":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/birb'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("*bird sounds.mp3*"), description=(""), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif something == "fox":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/fox'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
-    embed=discord.Embed(title=("a single tail fox aproves it."), description=("(I think)"), color=(botcolor))
+    embed=discord.Embed(title=("fox mouse"), description=("(I think)"), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif something == "koala":
     jdat = (http.request('GET', 'https://some-random-api.ml/img/koala'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("a"), description=(""), color=(botcolor))
     embed.set_image(url=(jsdata['link']))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   else:
     await ctx.send("error, please put a valid command")
@@ -191,6 +199,7 @@ async def gmeme(ctx, image, top_text, bottom_text):
   generating_meme=("https://api.memegen.link/images/custom/" + top_text + "/" + bottom_text + img_type + "?background=" + image)
   display_image=discord.Embed(title="Your meme is ready!", description="", color=botcolor)
   display_image.set_image(url=generating_meme)
+  embed.set_footer(text="Powered by memegen.link")
   await ctx.send(embed=display_image)
 
 #@bot.command()
@@ -216,32 +225,41 @@ async def gmeme(ctx, image, top_text, bottom_text):
 async def fact(ctx, factstuff):
   if factstuff == "cat":
     jdat = (http.request('GET', 'https://catfact.ninja/fact'))
+    #jdat = (http.request("GET", "https://some-random-api.ml/facts/cat"))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by catfact.ninja")
+    #embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif factstuff == "dog":
     jdat = (http.request('GET', 'https://some-random-api.ml/facts/dog'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif factstuff == "panda":
     jdat = (http.request('GET', 'https://some-random-api.ml/facts/panda'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by Some Random Api")
+    await ctx.send(embed=embed)
   elif factstuff == "fox":
     jdat = (http.request('GET', 'https://some-random-api.ml/facts/fox'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif factstuff == "bird":
     jdat = (http.request('GET', 'https://some-random-api.ml/facts/bird'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif factstuff == "koala":
     jdat = (http.request('GET', 'https://some-random-api.ml/facts/koala'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=("Did you know..."), description=(jsdata['fact']), color=(botcolor))
+    embed.set_footer(text="Powered by Some Random Api")
     await ctx.send(embed=embed)
   elif factstuff == "DEL":
     await ctx.send("Discord Extreme List, i don't know who is but, sounds like a nice name :)")
@@ -278,13 +296,14 @@ async def chucknorris(ctx):
     jsdata = (json.loads(jdat.data.decode('utf-8')))
     embed=discord.Embed(title=(jsdata['value']), url=(jsdata['url']), description=(""), color=(botcolor))
     embed.set_thumbnail(url=(jsdata['icon_url']))
+    embed.set_footer(text="Powered by chucknorris.io")
     await ctx.send(embed=embed)
   
 @bot.command()
 async def hug(ctx):
     jdat = (http.request('GET', 'https://some-random-api.ml/animu/hug'))
     jsdata = (json.loads(jdat.data.decode('utf-8')))
-    await ctx.send(jsdata['link'])
+    await ctx.send(jsdata['link'] + "\nPowered by Some Random Api")
 
 @bot.command()
 async def reddit(ctx):
@@ -298,9 +317,8 @@ async def reddit(ctx):
   jdat = (http.request('GET', 'https://meme-api.herokuapp.com/gimme'))
   jsdata = (json.loads(jdat.data.decode('utf-8')))
   if jsdata['nsfw'] == "true":
-    embed=discord.Embed(title=("This meme is NSFW"), description=("We're going to reload this command, sorry for the incombinient."), color=(botcolor))
+    embed=discord.Embed(title=("This meme is NSFW"), description=("Please, use " + prefix + "reddit again"), color=(botcolor))
     await ctx.send(embed=embed)
-    await ctx.send(prefix + "reddit")
   else:
     #Embed
     embed_two=discord.Embed(title=(jsdata['title']), url=(jsdata['postLink']), description=(""), color=(botcolor))
@@ -309,6 +327,7 @@ async def reddit(ctx):
     embed_two.add_field(name="Posted by:", value=("u/" + jsdata['author']), inline=(True))
     embed_two.add_field(name="Subreddit:", value=("r/" + jsdata['subreddit']), inline=(True))
     embed_two.set_image(url=jsdata['url'])
+    embed_two.set_footer(text="Powered by Dev Daksan's Meme API")
     await ctx.send(embed=embed_two)
 
 @bot.command()
@@ -316,15 +335,26 @@ async def meme(ctx, memetype):
   if memetype == "meme":
     ctx.send("Command deleted, if you want something similar, please try: " + prefix + "reddit")
   elif memetype == "reddit":
-    ctx.send("Command moved to \"" + prefix + "reddit")
+    ctx.send("Command moved to \"" + prefix + "reddit\"")
   elif memetype == "generator":
     embed=discord.Embed(title=("MeMe gEnErAtOr!!!"), description=("*(yay!)*"), color=(botcolor))
     embed.add_field(name="How to use?", value="Type " + prefix + "gmeme <template url> <top text> <bottom text>", inline=True)
     embed.add_field(name="Writing your meme", value="You can't type you meme just like that, if you like have characters like the '?' or the space ' ' you need to put special characters the guide will be here: https://git.io/JUrSj", inline=True)
     embed.add_field(name="Image types supported", value="Currently are allowed '.jpg', '.jpeg', '.webp' and '.png'")
+    #embed.set_footer(text="Powered by memegen.link")
     await ctx.send(embed=embed)
   else:
     await ctx.send("error, please put a valid command")
+
+#@bot.command()
+#async def w(ctx, *, query):
+#    jdatq = (http.request('GET', 'https://www.metaweather.com/api/location/search/?query=' + query))
+#    jsdataq = (json.loads(jdatq.data.decode('utf-8')))
+#    queryp = (http.request('GET', #'https://www.metaweather.com/api/location/' + jsdataq['woeid'] + '/'))
+#    qp = (json.loads(queryp.data.decode('utf-8')))
+#    embed=discord.Embed(title=(qp['value']), url=(jsdata['url']), description=(""), color=(botcolor))
+#    embed.set_thumbnail(url=(jsdata['icon_url']))
+#    await ctx.send(embed=embed)
 
 @bot.command()
 async def invite(ctx):
@@ -332,7 +362,7 @@ async def invite(ctx):
 
 @bot.command()
 async def issues(ctx):
-  await ctx.send("https://github.com/APIcord/issues")
+  await ctx.send("https://github.com/APIcord/discord/issues")
 
 @bot.command()
 async def about(ctx):
@@ -342,12 +372,12 @@ async def about(ctx):
   embed_two.add_field(name="Owner:", value=os.getenv("OWNER"), inline=True)
   embed_two.add_field(name="Prefix:", value=os.getenv("PREFIX"), inline=True)
   embed_two.add_field(name="Number of servers:", value=f"{len(bot.guilds)} servers", inline=True)
-  embed_two.set_footer(text="Powered by APIcord")
+  embed_two.set_footer(text="Bot software powered by APIcord")
   await ctx.send(embed=embed_two)
 
 @bot.command(aliases=["apicord", "cord"])
 async def credits(ctx):
-  embed=discord.Embed(title=("APIcord Alpha 3.0.1"), description=("Credits"), color=(botcolor))
+  embed=discord.Embed(title=("APIcord Alpha 3.1"), description=("Credits"), color=(botcolor))
   embed.add_field(name=("Creator and programmer"), value=("error#7900 (absucc)"), inline=(False))
   embed.add_field(name=("APIs"), value=("used in this project"), inline=(False))
   embed.add_field(name=("Coffee API"), value=("by **Alex Flipnote** \n (" + prefix + "img coffee)"), inline=(True))
@@ -357,7 +387,8 @@ async def credits(ctx):
   embed.add_field(name=("memegen.link"), value=("by **Jace Browning** \n (" + prefix + "gmeme/gm)"), inline=(True))
   #embed.add_field(name=("Fun Translations API"), value=("by **Fun Translations** \n (" + prefix + "funtranslations/fun_translations/ft)"), inline=(True))
   embed.add_field(name=("chucknorris.io"), value=("by **chucknorris.io** \n (" + prefix + "chucknorris)"), inline=(True))
-  embed.add_field(name=("Some Random Api"), value=("by **Seif Mansour**, **Taka Inzori**, **Excigma** & **Telk** \n (" + prefix + "img cat, " + prefix + "img panda, " + prefix + "img red panda, " + prefix + "img bird, " + prefix + "img fox, " + prefix + "img koala, " + prefix + "hug, " + prefix + "fact dog, " + prefix + "fact panda, " + prefix + "fact fox, " + prefix + "fact bird & " + prefix + "fact koala)"), inline=(False))
+  embed.add_field(name=("xkcd's JSON interface"), value=("by **Randall Munroe** \n (" + prefix + "xkcd)"), inline=(True))
+  embed.add_field(name=("Some Random Api"), value=("by **Seif Mansour**, **Taka Inzori**, **Excigma** & **Telk** \n (" + prefix + "img cat, " + prefix + "img panda, " + prefix + "img red panda, " + prefix + "img bird, " + prefix + "img fox, " + prefix + "img koala, " + prefix + "hug, " + prefix + "fact cat, " + prefix + "fact dog, " + prefix + "fact panda, " + prefix + "fact fox, " + prefix + "fact bird & " + prefix + "fact koala)"), inline=(False))
   embed.add_field(name="Libraries", value="used", inline=False)
   embed.add_field(name="discord.py", value="by **Rapptz and the discord.py community**", inline=True)
   embed.add_field(name="python-telegram-bot", value="by **python-telegram-bot community**", inline=True)
@@ -384,7 +415,7 @@ async def privacy(ctx):
 
 @bot.command()
 async def code(ctx):
-  await ctx.send("https://github.com/APIcord/discord")
+  await ctx.send("https://github.com/APIcord/discord\nhttps://gitlab.com/APIcord/discord (Mirror)")
 
 @bot.command()
 async def say(ctx, *, yourmessage):
@@ -403,6 +434,29 @@ async def purge(ctx, the_limit):
   embed=discord.Embed(title=":wastebucket::thumbsup:", description="Made", color=botcolor)
   await ctx.channel.purge(limit=the_limit)
   await ctx.send(embed=embed)
+
+@bot.command()
+async def xkcd(ctx, numberz):
+  def last_xkcd_comic_ever():
+    jdat = (http.request('GET', 'https://xkcd.com/info.0.json'))
+    jsdata = (json.loads(jdat.data.decode('utf-8')))
+    embed=discord.Embed(title=(jsdata["title"]), description="N° " + str(jsdata["num"]) + " | " + jsdata["month"] + "/" + jsdata["day"] + "/" + jsdata["year"], color=botcolor)
+    embed.set_image(url=jsdata["img"])
+    embed.set_footer(text="Powered by xkcd's JSON interface")
+    await ctx.send(embed=embed)
+  if numberz == "latest":
+    last_xkcd_comic_ever()
+  elif numberz == "last":
+    last_xkcd_comic_ever()
+  elif numberz == "current":
+    last_xkcd_comic_ever()
+  else:
+    jdat = (http.request('GET', 'https://xkcd.com/' + numberz + '/info.0.json'))
+    jsdata = (json.loads(jdat.data.decode('utf-8')))
+    embed=discord.Embed(title=(jsdata["title"]), description="N° " + str(jsdata["num"]) + " | " + jsdata["month"] + "/" + jsdata["day"] + "/" + jsdata["year"], color=botcolor)
+    embed.set_image(url=jsdata["img"])
+    embed.set_footer(text="Powered by xkcd's JSON interface")
+    await ctx.send(embed=embed)
 
 @bot.command(aliases=["hgang", "Hgang", "HGang", "H", "h", "jointhehgang"])
 async def joinhgang(ctx):
